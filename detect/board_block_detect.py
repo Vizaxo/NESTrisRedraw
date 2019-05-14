6587435aa13ec47a9fbd879035b05223d47f2eef
Dan Mason <psydm3@nottingham.ac.uk>
2019-05-14 18:20:32 +0100

    BlockLocations function now takes an image matrix rather than image path
    This is so that frame data can be passed into the function instead of a path to an image. Edited the test function accordingly, and also improved commenting.

--- detect/board_block_detect.py
+++ detect/board_block_detect.py
@@ -2,10 +2,13 @@ import cv2
 import numpy as np
 from timeit import default_timer as timer
 
+# Function to calculate a simple difference metric between two GBR tuples,
+# using the sum of absolute differences between each set of GBR values.
 def DifferenceOfTuple(t1, t2):
     diff = abs(t1[0] - t2[0]) + abs(t1[1] - t2[1]) + abs(t1[2] - t2[2])
     return diff
 
+# Function for printing a tagged board 
 def BoardPrint(board):
     for x in board:
         for y in range(len(x)):
@@ -15,10 +18,14 @@ def BoardPrint(board):
                 x[y] = str(x[y])
         print(" ".join(x))
 
-def BlockLocations(image_path, colours):
+# Function that takes in an image matrix of a tetris board, and returns a
+# matrix of integers representing that board. Each integer represents which
+# 'colour group' a block belongs to. 
+def BlockLocations(cv_image_matrix, colours):
+    # Generate a matrix of a blank, standard tetris board (10 x 20 blocks)
     board = [[" " for x in range(10)] for x in range(20)]
-    # Image read into an opencv image matrix (GBR colour format)
-    board_image = cv2.imread(image_path)
+    # Calculate information about image matrix
+    board_image = cv_image_matrix
     height,width,bpp = np.shape(board_image)
     block_height = height / 20
     block_width = width / 10
@@ -29,16 +36,24 @@ def BlockLocations(image_path, colours):
             y_start_pos = round(y * block_height)
             x_end_pos = round(x_start_pos + block_width - 1)
             y_end_pos = round(y_start_pos + block_height - 1)
+            # Takes the pixel region containing the current block
             current_block = board_image[y_start_pos : y_end_pos + 1,
                                         x_start_pos : x_end_pos + 1]
+            
+            # Takes a set of 4 (2 x 2) pixels from the center of a block
             block_middle = current_block[round(block_height / 2) : round((block_height / 2) + 2),
                                          round(block_width / 2) : round((block_width / 2) + 2)]
-            mean_rgb = np.array(cv2.mean(block_middle)[0:3])
+            
+            # Calculates the mean GBR value from the set of pixels. Uses a set of pixels instead of a
+            # single pixel to account for potential issues caused by noise or blurring of a frame. 
+            mean_gbr = np.array(cv2.mean(block_middle)[0:3])
             
             # old line using root means squared error - 2x slowdown
             # differences = [np.linalg.norm(x - mean_rgb) for x in colours]
-            
-            differences = [DifferenceOfTuple(mean_rgb, x) for x in colours]
+
+            # Takes the index of the closest colour of the current block, 
+            # tagging the 'colour group' it belongs to. 
+            differences = [DifferenceOfTuple(mean_gbr, x) for x in colours]
             board[y][x] = differences.index(min(differences))
 
     return board
--- detect/board_block_detect_test.py
+++ detect/board_block_detect_test.py
@@ -2,13 +2,18 @@ import numpy as np
 import os
 from board_block_detect import *
 
-# GBR values of the different colour groups 
+# Manually measured GBR values of the different colour groups 
 black_rgb = np.array((0,0,0))
 colour_1 = np.array((255,146,131))
 colour_2 = np.array((116,184,79))
 colour_3 = np.array((244,254,241))
-
 colours = [black_rgb, colour_1, colour_2, colour_3]
-# gets path to home directory of NESTrisRedraw
+
+# Gets path to home directory of NESTrisRedraw
 main_directory_path = os.path.dirname(os.path.dirname(__file__))
-BoardPrint(BlockLocations(os.path.join(main_directory_path, "resources", "images", "tetris.png"), colours))
+# Join the path to the image to the home directory path
+image_path = os.path.join(main_directory_path, "resources", "images", "tetris.png")
+# Loads image using opencv, into an GBR colour matrix.
+image_matrix = cv2.imread(image_path)
+# Tags all of the blocks in the image, then prints the output.
+BoardPrint(BlockLocations(image_matrix, colours))
